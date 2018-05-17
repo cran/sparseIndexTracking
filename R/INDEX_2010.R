3827d1144be66e421cd96664e2676698ab9fecbb
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2018-05-17 08:04:46 +0000

    version 0.1.0

---
+++ R/INDEX_2010.R
@@ -0,0 +1,14 @@
+#' Database of the net returns of the index S&P 500 and its underlying assets during the year 2010
+#'
+#' Database of the net returns of the index S&P 500 and its underlying assets during the year 2010.
+#' Only the assets that were in the index during the whole period are included.
+
+#' @docType data
+#'
+#' @usage data(INDEX_2010)
+#'
+#' @format A list of two \code{xts} objects, namely, \code{X} and \code{SP500}, corresponding to the net returns of the assets and the index, respectively.
+#'
+#' @keywords datasets
+#'
+"INDEX_2010"
